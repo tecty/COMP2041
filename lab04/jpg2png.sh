b29e8de84a535a3faf65cfd4c70128e9818bee18
Toby HUANG <619058900@qq.com>
2018-08-16 12:46:03 +1000

    Add error message to pass the autotest

--- lab04/jpg2png.sh
+++ lab04/jpg2png.sh
@@ -3,6 +3,12 @@ for fileName in * ; do
   if  echo $fileName | egrep "*.jpg"  2>&1 >/dev/null; then
     #statements
     fileNamePng=`echo $fileName |sed "s/.jpg/.png/g"`
-    convert "$fileName" "$fileNamePng"
+    if ls -- "$fileNamePng" >/dev/null 2>&1 
+    then
+      echo $fileNamePng "already exists" >&2
+    else 
+      convert "$fileName" "$fileNamePng"
+      rm -- "$fileName" 
+    fi
   fi
 done
