7d551770b988b145b12c0f554243ce56fcd0c7f7
Toby HUANG <619058900@qq.com>
2018-08-16 14:36:37 +1000

    remove the bug in mutiple dir.

--- lab04/tag_music.sh
+++ lab04/tag_music.sh
@@ -1,22 +1,30 @@
 #/bin/bash
 
-dirName=$@
 
 # change the directory to the directory we need to change id
-cd  "$dirName"
-# fetch the information from the directory name
-album=`basename "$PWD"`
-year=`basename "$album" | cut -d',' -f2 | cut -d' ' -f2`
+# cd  "$dirName"
 
-for musicFile in * ; do
-  # extract the information from the file name
-  track=`echo $musicFile | cut -d'-' -f1 | sed "s/ $//g"`
-  title=`echo $musicFile | cut -d'-' -f2 | sed "s/^ //g" | sed "s/ $//g"`
-  artist=`echo $musicFile | cut -d'-' -f3 | sed "s/^ //g" | sed "s/.mp3//g"`
 
-  # set the id
-  id3 -t "$title" -T "$track" -a "$artist" -A "$album" \
+
+for dir in "$@"; do
+  cd - >/dev/null
+  cd "$dir"
+  # fetch the information from the directory name
+  album=`basename "$PWD"`
+  year=`basename "$album" | cut -d',' -f2 | cut -d' ' -f2`
+
+  for musicFile in * ; do
+    # extract the information from the file name
+    track=`echo $musicFile | cut -d'-' -f1 | sed "s/ $//g"`
+    title=`echo $musicFile | cut -d'-' -f2 | sed "s/^ //g" | sed "s/ $//g"`
+    artist=`echo $musicFile | cut -d'-' -f3 | sed "s/^ //g" | sed "s/.mp3//g"`
+
+    # set the id
+    id3 -t "$title" -T "$track" -a "$artist" -A "$album" \
     -y "$year" "$musicFile" > /dev/null
 
-  # id3 -l "$musicFile"
+    # echo $musicFile
+
+    # id3 -l "$musicFile"
+  done
 done
