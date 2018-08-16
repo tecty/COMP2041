28beee48f9939ddb24638c678b9c502a72ec1e7d
Toby Huang <619058900@qq.com>
2018-08-16 12:36:18 +1000

    modify the jpg2png.sh

--- lab04/jpg2png.sh
+++ lab04/jpg2png.sh
@@ -3,6 +3,6 @@ for fileName in * ; do
   if  echo $fileName | egrep "*.jpg"  2>&1 >/dev/null; then
     #statements
     fileNamePng=`echo $fileName |sed "s/.jpg/.png/g"`
-    convert -- "$fileName" "$fileNamePng"
+    convert "$fileName" "$fileNamePng"
   fi
 done
