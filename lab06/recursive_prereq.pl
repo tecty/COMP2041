8a03e35cb1424bcf5e241557c20dacc0649a20f8
Toby HUANG <619058900@qq.com>
2018-08-30 14:44:44 +1000

    A try of challenge

--- lab06/recursive_prereq.pl
+++ lab06/recursive_prereq.pl
@@ -2,6 +2,8 @@
 
 use warnings;
 
+$root_course = $ARGV[0];
+
 sub find_pre {
   my (@file) = @_;
   # body...
@@ -15,18 +17,49 @@ sub find_pre {
         $pre=~ s/^ //g;
         my @courses= ($pre =~ /[A-Z]{4}[0-9]{4}/g);
         foreach my $course (@courses) {
-          print "$course\n";
+          if (! exists $visited{$course} && ! exists $unvisited{$course} ) {
+            # this course need to be visited, push to the list
+            push @unvisited ,$course;
+          }
+
         }
         last;
       }
   }
 }
 
+sub visit_course {
+  push @unvisited, $ARGV[0];
 
-$ug_url = "http://www.handbook.unsw.edu.au/postgraduate/courses/2018/$ARGV[0].html";
-$pg_url = "http://www.handbook.unsw.edu.au/undergraduate/courses/2018/$ARGV[0].html";
-open UG, "wget -q -O- $ug_url|" or die;
-open PG, "wget -q -O- $pg_url|" or die;
+  # use a queue to check which course we need to visit 
+  while (@unvisited) {
+    $course = shift @unvisited;
 
-find_pre(<UG>);
-find_pre(<PG>);
+    # this course will be visited
+    $visited{$course}  = 1;
+
+    $ug_url = "http://www.handbook.unsw.edu.au/postgraduate/courses/2018/$course.html";
+    $pg_url = "http://www.handbook.unsw.edu.au/undergraduate/courses/2018/$course.html";
+    open UG, "wget -q -O- $ug_url|" or die;
+    open PG, "wget -q -O- $pg_url|" or die;
+
+    find_pre(<UG>);
+    find_pre(<PG>);
+    # close those files
+    close UG;
+    close PG;
+  }
+
+
+  $unvisited{$course}  = 1;
+}
+
+
+visit_course($ARGV[0]);
+
+foreach my $key (sort keys %visited) {
+  if ($root_course ne  $key ) {
+    print "$key\n";
+  }
+
+}
