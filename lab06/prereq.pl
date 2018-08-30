4b9608b6d41a9ce6aedf456ac0a26d6e79d051b5
Toby HUANG <619058900@qq.com>
2018-08-30 14:59:59 +1000

    Remain Challenge need to modify

--- lab06/prereq.pl
+++ lab06/prereq.pl
@@ -6,18 +6,16 @@ sub find_pre {
   my (@file) = @_;
   # body...
   foreach my $line (@file) {
-      # $line =~ s/<.*?>//gi ;
-      # $line =~ s/\n//g;
-      # $line =~ s/\s//g;
       if ($line =~ /<p>Prerequisite[s]?:(.*?)<\/p>/ ) {
         # body...
         my ($pre)= $1;
         $pre=~ s/^ //g;
         my @courses= ($pre =~ /[A-Z]{4}[0-9]{4}/g);
-        foreach my $course (@courses) {
-          print "$course\n";
-        }
-        last;
+        # foreach my $course (@courses) {
+        #   print "$course\n";
+        # }
+        # print @courses;
+        return @courses;
       }
   }
 }
@@ -28,5 +26,14 @@ $pg_url = "http://www.handbook.unsw.edu.au/undergraduate/courses/2018/$ARGV[0].h
 open UG, "wget -q -O- $ug_url|" or die;
 open PG, "wget -q -O- $pg_url|" or die;
 
-find_pre(<UG>);
-find_pre(<PG>);
+push @courses , find_pre(<UG>);
+push @courses , find_pre(<PG>);
+
+foreach my $course (sort @courses) {
+  if ($course eq "") {
+    # don't print this line
+    next;
+  }
+
+  print "$course\n";
+}
--- lab06/whale_summary.pl
+++ lab06/whale_summary.pl
@@ -13,22 +13,25 @@ while ($line = <F>){
 
     $whale_specie = $1;
     # remove the head and tail ch we doesn't want
-    $whale_specie =~ s/^ //g;
     $whale_specie =~ s/\n//g;
     # remove messey space we doesn't want
     $whale_specie =~ s/[ ]+/ /g;
 
+    $whale_specie =~ s/^ //g;
+    $whale_specie =~ s/ $//g;
+    $whale_specie =~ s/s$//ig;
     $whale_specie = lc $whale_specie;
+    # print "$whale_specie\n";
 
-    print "$whale_specie\n";
-	#$podCount{$whale_specie} ++;
+    # print "$whale_specie\n";
+  	$podCount{$whale_specie} ++;
     # fetch the the numbers of the identify observations
     $line =~ /\s(\d+)\s/;
     $count{$whale_specie} += $1;
   }
 }
 
-print "\n";
+# print "\n";
 
 foreach $key (sort keys %count){
   print "$key observations: $podCount{$key} pods, $count{$key} individuals\n";
