af123746802fe48b1d86d079d09926634cca8ce7
Toby HUANG <619058900@qq.com>
2018-09-25 01:34:40 +1000

    merge some test i will fail

---
+++ ass1/tests/remove.sh
@@ -0,0 +1,16 @@
+./legit.pl init
+echo 1 >a
+echo 2 >b
+./legit.pl add a b
+./legit.pl commit -m "first commit"
+echo 3 >c
+echo 4 >d
+./legit.pl add c d
+./legit.pl rm --cached  a c
+./legit.pl commit -m "second commit"
+./legit.pl show 0:a
+./legit.pl show 1:a
+./legit.pl show :a
+
+# clean up
+rm a b c d 
