1e952fb09383c4fb119f5bb9c52367ef34bc9d6d
Toby HUANG <619058900@qq.com>
2018-09-25 23:34:46 +1000

    introduce another commit test

---
+++ ass1/tests/commit_again.sh
@@ -0,0 +1,16 @@
+./legit.pl init
+touch a b
+./legit.pl add a b
+./legit.pl commit -m "first commit"
+rm a
+./legit.pl commit -m "second commit"
+./legit.pl add a
+./legit.pl commit -m "second commit"
+./legit.pl rm --cached b
+./legit.pl commit -m "second commit"
+./legit.pl rm b
+./legit.pl add b
+./legit.pl rm b
+./legit.pl commit -m "third commit"
+./legit.pl rm b
+./legit.pl commit -m "fourth commit"
