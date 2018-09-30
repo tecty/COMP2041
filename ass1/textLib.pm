500373e26d3b48a33521273ab8b3da878e1e0a12
Toby HUANG <619058900@qq.com>
2018-10-01 05:00:21 +1000

    lcs is getted

--- ass1/textLib.pm
+++ ass1/textLib.pm
@@ -25,12 +25,78 @@ sub is_diff(\@\@) {
   return 0;
 }
 
+sub max_two {
+  # simple way to find max in two number
+  my ($a, $b) = @_;
+  return ($a > $b) ? $a : $b;
+}
 
 
 sub diff (\@\@) {
   # we introduce two array to diff
-  my ($a, $b) = @_;
-  # map {} @$a;
+  # using LCS algorithm
+  # output = dest - ref
+  my ($dest_ref ,$src_ref) = @_;
+  # create an array of content inputed, in this way, we can prevent the
+  # source array wouldn't be change
+  my @src = @$src_ref;
+  my @dest = @$dest_ref;
+
+  # unshift it to have better calculation property
+  unshift @src, "";
+  unshift @dest, "";
+
+  # array of the algorithm's output
+  my @lcs_array;
+  # init the array
+  for (my $x = 0; $x < @src; $x++) {
+    for (my $y = 0; $y < @dest; $y++) {
+      $lcs_array[$x][$y] = 0;
+    }
+  }
+
+
+  # first row and column is 0 as required
+  for (my $x = 1; $x < @src; $x++) {
+    for (my $y = 1; $y < @dest; $y++) {
+      if ($src[$x] eq $dest[$y]) {
+        # according to algo, arr[x][y] = arr[x-1][y-1]+1
+        $lcs_array[$x][$y] =$lcs_array[$x-1][$y-1] +1;
+      }
+      else{
+        $lcs_array[$x][$y] = max_two($lcs_array[$x - 1][$y], $lcs_array[$x][$y-1]);
+      }
+    }
+  }
+
+  # record the position of same string, which construct the lcs
+  my %eq_pos;
+  # travelback the array
+  my $x = @src -1;
+  my $y = @dest -1;
+  while ($lcs_array[$x][$y] != 0){
+    if ($lcs_array[$x][$y-1] == $lcs_array[$x][$y] ) {
+      # move up because this direction is approved
+      $y = $y -1;
+    }
+    elsif ($lcs_array[$x-1][$y] == $lcs_array[$x][$y]){
+      $x = $x -1;
+    }
+    else{
+      # both direction is block ==> this location has both sting is equal
+      $eq_pos{$x} = $y;
+      # move left and up
+      $x = $x-1; $y = $y -1;
+    }
+  }
+
+
+  # return the operation to construct the dest
+  # ret = dest - src;
+  my %ret;
+
+
+  return %eq_pos;
 }
 
 sub patch {
