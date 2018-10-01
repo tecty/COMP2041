1f1f82d738023fa8ded47df14214333c6919fc3e
Toby HUANG <619058900@qq.com>
2018-10-01 21:41:03 +1000

    add another test to test fastforward test

---
+++ ass1/myTest/fast_forward.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+./legit.pl init
+touch a
+./legit.pl add a
+./legit.pl commit -m "commit -0"
+./legit.pl branch another
+./legit.pl checkout another
+./legit.pl add b
+./legit.pl commit -m "commit -1"
+./legit.pl add c
+./legit.pl commit -m "commit -2"
+./legit.pl add d
+./legit.pl commit -m "commit -3"
+./legit.pl checkout master
+# try to fast forward merge
+./legit.pl merge another
+./legit.pl status 
