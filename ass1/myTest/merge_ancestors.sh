1e4470f01a89df50aa77786e489fdd2e99334095
Toby HUANG <619058900@qq.com>
2018-10-03 03:52:07 +1000

    fix the bugs in tests

--- ass1/myTest/merge_ancestors.sh
+++ ass1/myTest/merge_ancestors.sh
@@ -5,10 +5,13 @@ touch a
 ./legit.pl commit -m "commit -0"
 ./legit.pl branch another
 ./legit.pl checkout another
+touch b
 ./legit.pl add b
 ./legit.pl commit -m "commit -1"
+touch c
 ./legit.pl add c
 ./legit.pl commit -m "commit -2"
+touch d
 ./legit.pl add d
 ./legit.pl commit -m "commit -3"
 # try to fast forward merge
