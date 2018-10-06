2633c321fc49ae8cfaa6fc252e086d8b8126a012
Toby HUANG <619058900@qq.com>
2018-10-07 06:58:01 +1100

    another debug of identify sh

--- tests/test10/ls_identical.sh
+++ tests/test10/ls_identical.sh
@@ -4,11 +4,12 @@ a_files=`ls $1/*`;
 b_files=`ls $2/*`;
 same_files=();
 index=0;
-for file in "$a_files"
+for file in $a_files
 do
     b_file=`echo "${file}" | sed s/^$1/$2/`
+    # echo $b_file;
     if  `diff "${file}" "${b_file}"  > /dev/null 2>/dev/null` ;then
-        same_files[$index]=`echo "$file" | sed s?^$1/??` 
+        same_files[$index]=`echo $file | sed s?^$1/??` 
         # echo ${same_files[$index]}
         # echo $index
         ((index ++));
