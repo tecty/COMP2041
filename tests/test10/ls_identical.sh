ec573e18f152fe42b0a82dd25e2e995bbd6c9bbe
Toby Huang <619058900@qq.com>
2018-10-07 12:06:20 +1100

    pass all the tests

--- tests/test10/ls_identical.sh
+++ tests/test10/ls_identical.sh
@@ -14,7 +14,7 @@ do
     b_file=`echo "../$2/${file}"`
     # echo $b_file;
     if  `diff "${file}" "${b_file}"  > /dev/null 2>/dev/null` ;then
-        same_files[$index]=`echo $file | sed s?^$1/??`
+        same_files[$index]=`echo "$file" | sed s?^$1/??`
         # echo ${same_files[$index]}
         # echo $index
         ((index ++));
@@ -24,6 +24,6 @@ sorted=($(sort <<<"${same_files[*]}"))
 if ((index != 0)) ;then
     ((index--))
     for i in `seq 0 $index`; do
-            printf "%s\n" "${same_files[$i]}"
+        echo "${same_files[$i]}"
     done
 fi
