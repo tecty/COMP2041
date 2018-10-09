5135b227bb9fc64eadd803f6cf17607c8dfddbd9
Toby HUANG <619058900@qq.com>
2018-10-09 13:16:19 +1100

    add a functional ghost, try to make it shoot

--- labs/lab11/labs/lab11/adventure-script/adventure.js
+++ labs/lab11/labs/lab11/adventure-script/adventure.js
@@ -1,4 +1,91 @@
-(function() {
-   'use strict';
-   // code here
+class Fireball{
+  constructor(x,y){
+    
+  }
+}
+
+
+class Ghost {
+  constructor(el) {
+    this.theGhost = el;
+    this.slowMove = true;
+    // reset the original position 
+    this.theGhost.style.left = '10px';
+    this.theGhost.style.bottom  = '120px';
+    console.log(el.style);
+  }
+  getMoveSpeed(){
+    // fast move double the speed 
+    return this.slowMove? 50:75;
+  }
+  toggleMode(){
+    // switch the move speed 
+    this.slowMove = this.slowMove? false: true;
+  }
+  addpx(str, val){
+    return (parseInt(str.replace('px',''))+ val) + 'px';
+  }
+  left(){
+    this.theGhost.style.left  = this.addpx(
+      this.theGhost.style.left, - this.getMoveSpeed()
+    );
+  }
+  right(){
+    this.theGhost.style.left  = this.addpx(
+      this.theGhost.style.left, this.getMoveSpeed()
+    );
+  }
+  down(){
+    this.theGhost.style.bottom  = this.addpx(
+      this.theGhost.style.bottom, - this.getMoveSpeed()
+    );
+  }
+  up(){
+    this.theGhost.style.bottom  = this.addpx(
+      this.theGhost.style.bottom, this.getMoveSpeed()
+    );
+  }
+  shoot(){
+    
+  }
+}
+
+
+
+var ghost;
+
+function keyEventListener(e) {
+  switch (e.keyCode) {
+    case 37:
+      ghost.left();
+      break;
+    case 38:
+      ghost.up();
+      break;
+    case 40:
+      ghost.down();
+      break;
+    case 39:
+      ghost.right();
+      break;
+    case 90:
+      ghost.toggleMode();
+      break;
+    case 88:
+      ghost.shoot();
+      break;
+    default:
+      // console.log('I got '+ e.keyCode);
+      // don't handle normal situation 
+      break;
+  }
+}
+
+
+
+(function () {
+  'use strict';
+  // bind the key action 
+  document.onkeydown = keyEventListener;
+  ghost = new Ghost(document.getElementById('player'));
 }());
--- labs/lab11/labs/lab11/expand/expand.js
+++ labs/lab11/labs/lab11/expand/expand.js
@@ -1,4 +1,27 @@
-(function() {
+function expand(){
+  var foldTarget = this.childNodes[1].id+'-content';
+  this.childNodes[1].innerHTML = 'expand_less';
+  // change the onclick function back to fold 
+  this.onclick= fold;
+  // display this content 
+  document.getElementById(foldTarget).style.display = null;
+}
+
+
+function fold() {
+  // find the item
+  var foldTarget = this.childNodes[1].id+'-content';
+  this.childNodes[1].innerHTML = 'expand_more';
+  this.onclick = expand;
+  // change the style to display none 
+  document.getElementById(foldTarget).style.display = 'none';
+}
+
+
+(function () {
   'use strict';
-  // TODO: Write some js
+  var btns = document.getElementsByClassName('expand-collapse-btn');
+  [...btns].forEach((el)=>{
+    el.onclick = fold;
+  });
 }());
