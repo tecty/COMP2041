da82b1a6ba9b9887457ed7dfe6e6a4e34bb5fa70
Toby HUANG <619058900@qq.com>
2018-10-09 11:52:53 +1100

    finish the collapse

--- labs/lab11/labs/lab11/collapse/collapse.js
+++ labs/lab11/labs/lab11/collapse/collapse.js
@@ -1,4 +1,19 @@
-(function() {
+function fold() {
+  var foldTarget  = this.childNodes[1].id+'-content';
+  this.childNodes[1].innerHTML = 'expand_more';
+  // console.log('try to fold '+ foldTarget);
+  var target = document.getElementById(foldTarget);
+  target.remove();
+}
+
+
+(function () {
   'use strict';
   // TODO: Write some js
+  var btns = document.getElementsByClassName('expand-collapse-btn');
+  [...btns].forEach((el)=>{
+    // console.log(el.childNodes);
+    el.onclick = fold;
+  });
+  // console.log('imhere');
 }());
