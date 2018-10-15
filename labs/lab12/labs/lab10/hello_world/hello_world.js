31e4c6bd7cf4bed9cdc16501de6a6301211d486e
Toby HUANG <619058900@qq.com>
2018-10-15 21:31:02 +1100

    add lab 12

---
+++ labs/lab12/labs/lab10/hello_world/hello_world.js
@@ -0,0 +1,11 @@
+// write a program to prepend a string passed by the function
+// with the word, with a space ie: "Hello "
+// should return the new string.
+
+function hello(name) {
+
+   // ADD YOUR CODE HERE
+   return name;
+}
+
+module.exports = hello;
