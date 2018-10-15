40ffbcc5d76f9c209ed7538a5c3b1140c26591d3
Toby HUANG <619058900@qq.com>
2018-10-15 23:18:43 +1100

    try to fetch both lists

--- labs/lab12/labs/lab12/fetch_and_list_2/fetch.js
+++ labs/lab12/labs/lab12/fetch_and_list_2/fetch.js
@@ -1,4 +1,29 @@
-(function() {
+
+(async function () {
     'use strict';
 
- }());
+    let out = document.getElementById('output');
+    // fetch the users 
+    let users = await fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json())
+    
+    
+    // fetch the posts 
+    let posts = await fetch('https://jsonplaceholder.typicode.com/posts').then(r => r.json());
+
+    posts.forEach(p => {
+        console.log(p);
+    });
+
+    users.forEach(u => {
+        out.insertAdjacentHTML('beforeend',`
+            <div style='clear:both'>
+                <h2>${ u.name }</h2>
+                <p>${ u.company.catchPhrase }</p>
+            </div>
+        `);
+        // console.log(u);
+    }); 
+
+
+}());
+
