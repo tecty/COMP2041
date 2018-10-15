89e383ce5a320edd1b89c5583d0331fc5751244a
Toby HUANG <619058900@qq.com>
2018-10-16 10:29:33 +1100

    finish the fetch photo

--- labs/lab12/labs/lab12/pretty_pictures/fetch.js
+++ labs/lab12/labs/lab12/pretty_pictures/fetch.js
@@ -1,4 +1,41 @@
+function getPhotoUrl() {
+    return fetch('https://picsum.photos/200/300/?random');
+}
+
+Date.prototype.timeNow = function () {
+    return ((this.getHours() < 10)?'0':'') + this.getHours() +':'+ 
+        ((this.getMinutes() < 10)?'0':'') + this.getMinutes();
+};
+
+function more(){
+    // get the output div
+    let output = document.getElementById('output');
+    // clear all the photo currently display 
+    [...output.getElementsByClassName('img-post')].forEach(el=>{
+       el.remove(); 
+    });
+
+    // set up loading to true 
+    let loading = document.getElementById('loading');
+    loading.style.display = null;
+
+    Promise.all([getPhotoUrl(), getPhotoUrl(), getPhotoUrl(), getPhotoUrl(), getPhotoUrl()])
+        .then(a => {
+            a.forEach(el => {
+                output.insertAdjacentHTML('beforeend', `
+                <div class = "img-post">
+                    <img src="${el.url}" />
+                    <p>Fetched at ${(new Date()).timeNow()}</p>
+                </div>`
+            );});
+        // cancel the loading state 
+        loading.style.display = 'none';
+    });
+}
+
 (function () {
     'use strict';
-
+    more();
+    // attach the more function to the more btn 
+    document.getElementById('more').onclick = more;
 }());
