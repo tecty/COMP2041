3fe4cdee2de9284ef071129dedaff69667e1332b
Toby HUANG <619058900@qq.com>
2018-10-15 21:50:38 +1100

    first fetch is finished

--- labs/lab12/labs/lab12/fetch_and_list/fetch.js
+++ labs/lab12/labs/lab12/fetch_and_list/fetch.js
@@ -1,4 +1,17 @@
 (function () {
     'use strict';
 
+    let out = document.getElementById('output');
+    // url https://jsonplaceholder.typicode.com/users
+    fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json())
+    .then(users => users.forEach(u => {
+        out.insertAdjacentHTML('beforeend',`
+            <div style='clear:both'>
+                <h2>${ u.name }</h2>
+                <p>${ u.company.catchPhrase }</p>
+            </div>
+        `);
+        // console.log(u);
+    })) ; 
 }());
+
