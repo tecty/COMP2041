147c7982df278bf7572b0d0b5c234d479b397842
Toby HUANG <619058900@qq.com>
2018-10-16 09:41:24 +1100

    finish the post fecth

--- labs/lab12/labs/lab12/fetch_and_list_2/fetch.js
+++ labs/lab12/labs/lab12/fetch_and_list_2/fetch.js
@@ -4,21 +4,31 @@
 
     let out = document.getElementById('output');
     // fetch the users 
-    let users = await fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json())
+    let users = await fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
+    // assign an empty array for each user 
+    users.forEach((u)=> {
+        u.posts = [];
+    });
     
     
     // fetch the posts 
     let posts = await fetch('https://jsonplaceholder.typicode.com/posts').then(r => r.json());
 
     posts.forEach(p => {
-        console.log(p);
+        users.find(u=> u.id == p.userId).posts.push(p.title);
     });
 
     users.forEach(u => {
+        let postLi="";
+        console.log(u);
+        u.posts.forEach(p => postLi += `<li class="post">${p}</li>`);
+        
+        
         out.insertAdjacentHTML('beforeend',`
-            <div style='clear:both'>
+            <div class = "user">
                 <h2>${ u.name }</h2>
                 <p>${ u.company.catchPhrase }</p>
+                <ul class = "post">${postLi}</ul>
             </div>
         `);
         // console.log(u);
