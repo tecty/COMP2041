c8f4440518305a7dbb9f52fb0c4be4072a085194
Toby HUANG <619058900@qq.com>
2018-10-16 11:47:05 +1100

    finish the tabs

--- labs/lab12/labs/lab12/tabs/tabs.js
+++ labs/lab12/labs/lab12/tabs/tabs.js
@@ -1,4 +1,44 @@
+var content;
+var navLinks = [...document.getElementsByClassName('nav-link')];
+function renderSummary(pl) {
+    let ret ='';
+    Object.keys(pl.summary).forEach(key => {
+        ret +=`<li><b>${key}:</b> ${pl.summary[key]}</li>`;
+    });
+    return ret;
+}
+
+function refreshContent(e) {
+    // clear all the active class 
+    navLinks.forEach(el => {
+        el.classList.remove('active');
+    });
+    e.srcElement.classList.add('active');
+    
+    let elId = e.srcElement.id;
+    // parse as int 
+    let id = parseInt(elId.replace('tab-',''));
+
+    let thePlanet = content[id-1];
+
+    // console.log(thePlanet);
+    let info = document.getElementById('information');
+    info.innerHTML = `
+        <h2> ${thePlanet.name} </h2>
+        <hr>
+        <p>${thePlanet.details}</p>
+        <ul>${renderSummary(thePlanet)}</ul>
+    `;
+    
+}   
+
+
+
+
 (function () {
     'use strict';
-
+    fetch('planets.json').then(r => r.json()).then(r => content = r);
+    navLinks.forEach(l => {
+        l.onclick = refreshContent;
+    });
 }());
