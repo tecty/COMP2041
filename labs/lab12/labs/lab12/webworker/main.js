a814a8cd3b05fb3101691105aeb699a47e695888
Toby HUANG <619058900@qq.com>
2018-10-16 14:11:51 +1100

    finish the web worker

--- labs/lab12/labs/lab12/webworker/main.js
+++ labs/lab12/labs/lab12/webworker/main.js
@@ -1,3 +1,9 @@
 (function() {
-
+    let catEl = document.getElementById('cat');
+    let w = new Worker('worker.js');
+    // set the worker basic message 
+    w.postMessage('https://api.thecatapi.com/v1/images/search?&mime_types=image/gif');
+    
+    // set the update src 
+    w.onmessage = r => catEl.src = r.data;
 }());
--- labs/lab12/labs/lab12/webworker/worker.js
+++ labs/lab12/labs/lab12/webworker/worker.js
@@ -1,1 +1,16 @@
 // your web worker goes here.
+function updateCatUrl(url){
+    fetch(url)
+    .then(r=> r.json())
+    .then(r=> r[0].url)
+    .then(
+        // assign the cat el to the url 
+        url => postMessage(url)
+    );
+}
+
+
+onmessage = function(e){
+    updateCatUrl(e.data);
+    setInterval(()=>updateCatUrl(e.data), 5000);
+};
